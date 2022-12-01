d831bd6ff3b563ff570173eef304709617307011
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-12-01 17:28:07 +0300

    Добавлен фильтр для Докторов.

---
+++ MedicalSystemApplication/src/main/java/filters/DoctorFilter.java
@@ -0,0 +1,69 @@
+package filters;
+
+import dto.DoctorDTO;
+import helpers.DateUtil;
+import model.Doctor;
+
+public class DoctorFilter implements Filter{
+
+	@Override
+	public Boolean test(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+		
+		try 
+		{
+			Boolean flag = true;
+			Doctor d = (Doctor)o1;
+			DoctorDTO dto = (DoctorDTO)o2;
+			
+			if(!dto.getUser().getFirstname().equals(""))
+			{
+				if(!d.getFirstname().toLowerCase().contains(dto.getUser().getFirstname().toLowerCase()))
+				{
+					flag = false;
+				}
+			}
+			
+			if(!dto.getUser().getLastname().equals(""))
+			{
+				if(!d.getLastname().toLowerCase().contains(dto.getUser().getLastname().toLowerCase()))
+				{
+					flag = false;
+				}
+			}
+			
+			if(dto.getAverageRating() != 0)
+			{
+				if(Math.abs(d.getAverageRating() - dto.getAverageRating()) > 1f)
+				{
+					flag = false;
+				}			
+			}
+			
+			if(!dto.getType().equals(""))
+			{
+				if(!d.getType().toLowerCase().contains(dto.getType().toLowerCase()))
+				{
+					flag = false;
+				}
+			}
+			
+			if(dto.getShiftEnd() != null)
+			{
+				if(!d.IsFreeOn(DateUtil.getInstance().getDate(dto.getShiftEnd(),"dd-MM-yyyy")))
+				{
+					flag = false;
+				}
+			}
+			
+			return flag;
+			
+		}
+		catch(Exception e) 
+		{
+			return false;
+		}
+		
+	}
+
+}
