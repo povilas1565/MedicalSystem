a16172ef3136b5287d62eb4ede8e9fb18f4d0ae0
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-12-01 17:16:52 +0300

    Добавлен CentreFilterDTO.

---
+++ MedicalSystemApplication/src/main/java/dto/CentreFilterDTO.java
@@ -0,0 +1,68 @@
+package dto;
+
+public class CentreFilterDTO {
+
+	private String name;
+    private String address;
+    private String city;
+    private String state;
+    private float rating;
+    
+    
+  
+	public CentreFilterDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CentreFilterDTO(String name, String address, String city, String state, float rating) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.rating = rating;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public float getRating() {
+		return rating;
+	}
+
+	public void setRating(float rating) {
+		this.rating = rating;
+	}
+    
+ 	
+}
