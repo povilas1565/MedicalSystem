a73cb3c7eb7ea1d5df47312aab7f675235c89321
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-12-01 17:30:34 +0300

    Добавлен репозиторий для Пользователя.

---
+++ MedicalSystemApplication/src/main/java/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import model.User;
+import model.User.UserRole;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+	
+	public User findByEmailAndDeleted(String email, Boolean deleted);
+	
+	public List<User> findAllByRole(UserRole role);
+
+}
