899eb1b3fd6bfa059231e9bfee5479d718c25a5f
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-12-01 17:16:01 +0300

    Добавлена cущность Appointment.

---
+++ MedicalSystemApplication/src/main/java/dto/AppointmentDTO.java
@@ -0,0 +1,238 @@
+package dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import helpers.DateUtil;
+import model.*;
+import model.Appointment.AppointmentType;
+
+public class AppointmentDTO {
+
+	private String date;
+	private String endDate;
+	private String patientEmail;
+	private String centreName;
+	private String typeOfExamination;
+	private String startTimestamp;
+	private String newDate;
+	private String newEndDate;
+	
+	private Boolean done = false;
+	private Boolean predefined = false;
+	private Boolean confirmed = true;
+	
+	private int hallNumber;
+	private long duration;
+	private float price;
+	private int version;
+	
+	private List<String> doctors;
+	private AppointmentType type;
+	
+	public AppointmentDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AppointmentDTO(String date, String patientEmail, String centreName, int hallNumber, List<String> doctors,
+			long duration, String priceslist, AppointmentType type) {
+		super();
+		this.date = date;
+		this.patientEmail = patientEmail;
+		this.centreName = centreName;
+		this.hallNumber = hallNumber;
+		this.doctors = doctors;
+		this.duration = duration;
+		this.typeOfExamination = priceslist;
+		this.type = type;
+		this.done = false;
+	}
+	
+	public AppointmentDTO(Appointment appointment)
+	{
+		this.date = DateUtil.getInstance().getString(appointment.getDate(),"dd-MM-yyyy HH:mm");
+		if(appointment.getEndDate() != null)
+			this.endDate = DateUtil.getInstance().getString(appointment.getEndDate(), "dd-MM-yyyy HH:mm");
+		if(appointment.getPatient() != null)
+			this.patientEmail = appointment.getPatient().getEmail();
+		this.centreName = appointment.getCentre().getName();
+		this.hallNumber = appointment.getHall().getNumber();
+		this.doctors = new ArrayList<String>();
+		for(Doctor doc : appointment.getDoctors())
+		{
+			doctors.add(doc.getEmail());
+		}
+		this.duration = appointment.getDuration();
+		if(appointment.getPriceslist() != null)
+		{
+			this.typeOfExamination = appointment.getPriceslist().getTypeOfExamination();
+			this.price = appointment.getPriceslist().getPrice();
+		}
+		this.type = appointment.getAppointmentType();
+		this.version = appointment.getVersion();
+		this.done = appointment.getDone();
+		this.predefined = appointment.getPredefined();
+		this.confirmed = appointment.getConfirmed();
+		if(appointment.getNewDate() != null)
+			this.newDate = DateUtil.getInstance().getString(appointment.getNewDate(), "dd-MM-yyyy HH:mm");
+		if(appointment.getNewEndDate() != null)
+			this.newDate = DateUtil.getInstance().getString(appointment.getNewEndDate(), "dd-MM-yyyy HH:mm");
+	}
+
+	public AppointmentDTO(AppointmentRequest appointment)
+	{
+		this.date = DateUtil.getInstance().getString(appointment.getDate(),"dd-MM-yyyy HH:mm");
+		this.startTimestamp = DateUtil.getInstance().getString(appointment.getTimestamp(), "dd-MM-yyyy HH:mm");
+		this.patientEmail = appointment.getPatient().getEmail();
+		this.centreName = appointment.getCentre().getName();
+		if(appointment.getHall() != null)
+			this.hallNumber = appointment.getHall().getNumber();
+		this.doctors = new ArrayList<String>();
+		for(Doctor doc : appointment.getDoctors())
+		{
+			doctors.add(doc.getEmail());
+		}
+		this.typeOfExamination = appointment.getPriceslist().getTypeOfExamination();
+		this.price = appointment.getPriceslist().getPrice();
+		this.type = appointment.getAppointmentType();
+	}
+	
+	
+
+	public Boolean getConfirmed() {
+		return confirmed;
+	}
+
+	public void setConfirmed(Boolean confirmed) {
+		this.confirmed = confirmed;
+	}
+
+	public String getNewEndDate() {
+		return newEndDate;
+	}
+
+	public void setNewEndDate(String newEndDate) {
+		this.newEndDate = newEndDate;
+	}
+
+	public String getNewDate() {
+		return newDate;
+	}
+
+	public void setNewDate(String newDate) {
+		this.newDate = newDate;
+	}
+
+	public Boolean getPredefined() {
+		return predefined;
+	}
+
+	public void setPredefined(Boolean predefined) {
+		this.predefined = predefined;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getStartTimestamp() {
+		return startTimestamp;
+	}
+
+	public void setStartTimestamp(String startTimestamp) {
+		this.startTimestamp = startTimestamp;
+	}
+
+	public int getVersion() {
+		return version;
+	}
+
+	public void setVersion(int version) {
+		this.version = version;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getPatientEmail() {
+		return patientEmail;
+	}
+
+	public void setPatientEmail(String patientEmail) {
+		this.patientEmail = patientEmail;
+	}
+
+	public String getCentreName() {
+		return centreName;
+	}
+
+	public void setCentreName(String centreName) {
+		this.centreName = centreName;
+	}
+
+	public int getHallNumber() {
+		return hallNumber;
+	}
+
+	public void setHallNumber(int hallNumber) {
+		this.hallNumber = hallNumber;
+	}
+
+	public List<String> getDoctors() {
+		return doctors;
+	}
+
+	public void setDoctors(List<String> doctors) {
+		this.doctors = doctors;
+	}
+
+	public long getDuration() {
+		return duration;
+	}
+
+	public void setDuration(long duration) {
+		this.duration = duration;
+	}
+	
+	public String getTypeOfExamination() {
+		return typeOfExamination;
+	}
+
+	public void setTypeOfExamination(String priceslist) {
+		this.typeOfExamination = priceslist;
+	}
+
+	public AppointmentType getType() {
+		return type;
+	}
+
+	public void setType(AppointmentType type) {
+		this.type = type;
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+}
---
+++ MedicalSystemApplication/src/main/java/model/Appointment.java
@@ -0,0 +1,321 @@
+package model;
+
+import java.util.ArrayList;
+
+import java.util.Date;
+import java.util.List;
+import javax.persistence.*;
+
+
+@Entity
+public class Appointment 
+{
+	public enum AppointmentType{
+		Examination
+	}
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name= "startingDateAndTime")
+	private Date date;
+	
+	@Column(name= "endingDateAndTime")
+	private Date endDate;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Hall hall;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "patient_id")
+	private Patient patient;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "centre_id")
+	private Centre centre;
+	
+	@Column(name = "duration", nullable = true)
+	private long duration;
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	private List<Doctor> doctors;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "priceslist_id")
+	private Priceslist priceslist;
+	
+	@Column(name = "appointmentType")
+	private AppointmentType appointmentType;
+	
+	@Column(name="predefined")
+	private Boolean predefined = false;
+
+	@Column(name="done")
+	private Boolean done = false;
+	
+	@Column(name="confimred")
+	private Boolean confirmed = false;
+
+	private Date newDate;
+	private Date newEndDate;
+
+	@Version
+	private Integer version;
+	
+	public Appointment() {
+		super();
+		this.doctors = new ArrayList<>();
+		this.confirmed = true;
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Appointment(Date date, Hall hall, Patient patient, Centre centre, long duration,
+                       Priceslist priceslist, AppointmentType appointmentType) {
+		super();
+		this.date = date;
+		this.hall = hall;
+		this.patient = patient;
+		this.centre = centre;
+		this.duration = duration;
+		this.doctors = new ArrayList<>();
+		this.priceslist = priceslist;
+		this.appointmentType = appointmentType; 
+		this.confirmed = true;
+	}
+	
+	
+
+    public Boolean getConfirmed() {
+		return confirmed;
+	}
+
+
+	public void setConfirmed(Boolean confirmed) {
+		this.confirmed = confirmed;
+	}
+
+
+	public Date getNewEndDate() {
+        return newEndDate;
+    }
+
+    public void setNewEndDate(Date newEndDate) {
+        this.newEndDate = newEndDate;
+    }
+
+    public Date getEndDate() {
+		return endDate;
+	}
+
+	public Date getNewDate() {
+		return newDate;
+	}
+
+	public void setNewDate(Date newDate) {
+		this.newDate = newDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+
+	public Integer getVersion() {
+		return version;
+	}
+
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
+
+	public Boolean getPredefined() {
+		return predefined;
+	}
+
+
+	public void setPredefined(Boolean predefined) {
+		this.predefined = predefined;
+	}
+
+
+	public void setPriceslist(Priceslist priceslist) {
+		this.priceslist = priceslist;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public Hall getHall() {
+		return hall;
+	}
+
+	public void setHall(Hall hall) {
+		this.hall = hall;
+	}
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public Centre getCentre() {
+		return centre;
+	}
+
+	public void setCentre(Centre centre) {
+		this.centre = centre;
+	}
+
+	public long getDuration() {
+		return duration;
+	}
+
+	public void setDuration(long duration) {
+		this.duration = duration;
+	}
+
+	public List<Doctor> getDoctors() {
+		return doctors;
+	}
+
+	public void setDoctors(List<Doctor> doctors) {
+		this.doctors = doctors;
+	}
+
+	public Priceslist getPriceslist() {
+		return priceslist;
+	}
+
+	public void setPricelist(Priceslist priceslist) {
+		this.priceslist = priceslist;
+	}
+
+	public AppointmentType getAppointmentType() {
+		return appointmentType;
+	}
+
+	public void setAppointmentType(AppointmentType appointmentType) {
+		this.appointmentType = appointmentType;
+	}
+
+	public Boolean getDone() {
+		return done;
+	}
+
+	public void setDone(Boolean done) {
+		this.done = done;
+	}
+
+	public static class Builder
+	{
+		private Date date;
+		private Date endDate;
+		private Hall hall;
+		private Patient patient;
+		private Centre centre;
+		private Priceslist priceslist;
+		private List<Doctor> doctors;
+		private AppointmentType appointmentType;
+		private long duration;
+		
+		public Builder(Date date)
+		{
+			this.date = date;
+			doctors = new ArrayList<>();
+		}
+		
+		public Builder withHall(Hall hall)
+		{
+			this.hall = hall;
+			
+			return this;
+		}
+
+		public Builder withPatient(Patient patient)
+		{
+			this.patient = patient;
+			
+			return this;
+		}
+		
+		public Builder withCentre(Centre centre)
+		{
+			this.centre = centre;
+			
+			return this;
+		}
+		
+		public Builder withEndingDate(Date date)
+		{
+			this.endDate = date;
+			
+			return this;
+		}
+		
+		
+		public Builder withDoctors(ArrayList<Doctor> doctors)
+		{
+			this.doctors = doctors;
+			
+			return this;
+		}
+		
+		public Builder withPriceslist(Priceslist priceslist)
+		{
+			this.priceslist = priceslist;
+			
+			return this;
+		}
+		
+		public Builder withType(AppointmentType appointmentType)
+		{
+			this.appointmentType = appointmentType;
+			
+			return this;
+		}
+
+		public Builder withDuration(long duration) {
+			this.duration = duration;
+			return this;
+		}
+		
+		public Appointment build()
+		{
+			Appointment app = new Appointment();
+			app.setDate(this.date);
+			app.setPatient(this.patient);
+			app.setCentre(this.centre);
+			app.setHall(this.hall);
+			app.setDoctors(this.doctors);
+			app.setPricelist(priceslist);
+			app.setAppointmentType(this.appointmentType);
+			app.setDuration(this.duration);
+			app.setEndDate(this.endDate);
+			return app;
+		}
+
+
+    }
+	
+}
