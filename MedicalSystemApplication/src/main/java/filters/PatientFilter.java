ab02a66594a1bffe4f65ce2935ffe82483cc0a77
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-12-01 17:29:54 +0300

    Добавлен фильтр для Пациентов.

---
+++ MedicalSystemApplication/src/main/java/filters/PatientFilter.java
@@ -0,0 +1,40 @@
+package filters;
+
+import dto.UserDTO;
+import model.Patient;
+
+public class PatientFilter implements Filter{
+
+	
+	@Override
+	public Boolean test(Object o1, Object o2) {
+		// TODO Auto-generated method stub
+		try {
+			Patient p = (Patient)o1;
+			UserDTO d = (UserDTO)o2;
+			Boolean flag = true;
+			if(!d.getFirstname().equals(""))
+			{
+				if(!p.getFirstname().toLowerCase().contains(d.getFirstname().toLowerCase()))
+				{
+					flag = false;
+				}
+			}
+			if(!d.getLastname().equals(""))
+			{
+				if(!p.getLastname().toLowerCase().contains(d.getLastname().toLowerCase()))
+				{
+					flag = false;
+				}				
+			}
+			
+			return flag;
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+}
