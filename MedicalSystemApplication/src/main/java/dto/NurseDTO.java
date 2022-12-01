5c60129718107028e412a5940d52a8f222bf5b1a
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-12-01 14:55:45 +0300

    Добавлена сущность Nurse.

---
+++ MedicalSystemApplication/src/main/java/dto/NurseDTO.java
@@ -0,0 +1,94 @@
+package dto;
+
+import helpers.DateUtil;
+import model.Nurse;
+import model.User;
+
+public class NurseDTO {
+
+	private UserDTO user;
+	private String type;
+	private String shiftStart;
+	private String shiftEnd;
+	private String centreName;
+
+	
+	public NurseDTO()
+	{
+		super();
+	}
+
+	public NurseDTO(UserDTO user, String type, String shiftStart, String shiftEnd, String centreName) {
+		this.user = user;
+		this.type = type;
+		this.shiftStart = shiftStart;
+		this.shiftEnd = shiftEnd;
+		this.centreName = centreName;
+	}
+
+	public NurseDTO(Nurse n)
+	{
+		UserDTO dto = new UserDTO();
+		dto.setFirstname(n.getFirstname());
+		dto.setLastname(n.getLastname());
+		dto.setEmail(n.getEmail());
+		dto.setCity(n.getCity());
+		dto.setPhone(n.getPhone());
+		dto.setState(n.getState());
+		dto.setDate_of_birth(n.getDate_of_birth());
+		dto.setRole(User.UserRole.Nurse);
+		this.user = dto;
+
+		if(n.getCentre() != null)
+		{
+			this.centreName = n.getCentre().getName();
+		}
+		else
+		{
+			this.centreName = "N/A";
+		}
+		this.type = n.getType();
+		this.shiftStart = DateUtil.getInstance().getString(n.getShiftStart(),"HH:mm");
+		this.shiftEnd = DateUtil.getInstance().getString(n.getShiftEnd(),"HH:mm");
+	}
+
+	public UserDTO getUser() {
+		return user;
+	}
+
+	public void setUser(UserDTO user) {
+		this.user = user;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getShiftStart() {
+		return shiftStart;
+	}
+
+	public void setShiftStart(String shiftStart) {
+		this.shiftStart = shiftStart;
+	}
+
+	public String getShiftEnd() {
+		return shiftEnd;
+	}
+
+	public void setShiftEnd(String shiftEnd) {
+		this.shiftEnd = shiftEnd;
+	}
+
+	public String getCentreName() {
+		return centreName;
+	}
+
+	public void setCentreName(String centreName) {
+		this.centreName = centreName;
+	}
+}
---
+++ MedicalSystemApplication/src/main/java/model/Nurse.java
@@ -0,0 +1,235 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.*;
+
+import dto.NurseDTO;
+import helpers.DateUtil;
+import helpers.UserBuilder;
+
+@Entity
+public class Nurse extends User{
+
+
+	@Column(name = "type", nullable = true)
+	private String type;
+
+	@Column(name = "shiftStart")
+    private Date shiftStart;
+	
+    @Column(name = "shiftEnd")
+    private Date shiftEnd;
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Centre centre;
+    
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Prescription> prescriptions;
+
+	@OneToMany(fetch = FetchType.LAZY)
+	private List<Vacation> vacations;
+
+
+    public Nurse() {
+    }
+
+
+    public Nurse(String password, String email, String firstname, String lastname, String city, String state, String date_of_birth, String phone) {
+        super(password, email, firstname, lastname, city, state, date_of_birth, phone, UserRole.Nurse);
+        this.prescriptions = new ArrayList<>();
+		this.vacations = new ArrayList<>();
+        this.setIsFirstLog(true);
+    }
+    
+    public Nurse(User user)
+    {
+    	super(user);   
+    	this.prescriptions = new ArrayList<>();
+    	this.vacations = new ArrayList<>();
+        this.setIsFirstLog(true);
+    }
+
+	public Nurse(NurseDTO dto)
+	{
+		super(dto.getUser());
+		this.setRole(UserRole.Doctor);
+		this.setIsFirstLog(true);
+		this.shiftStart = DateUtil.getInstance().getDate(dto.getShiftStart(), "HH:mm");
+		this.shiftEnd = DateUtil.getInstance().getDate(dto.getShiftEnd(), "HH:mm");
+		this.vacations = new ArrayList<>();
+	}
+
+	public Boolean IsFreeOn(Date date)
+	{
+		for(Vacation v: vacations)
+		{
+			Date start = v.getStartDate();
+			Date end = v.getEndDate();
+
+			if(date.after(start) && date.before(end))
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	public List<Vacation> getVacations() {
+		return vacations;
+	}
+
+	public void setVacations(List<Vacation> vacations) {
+		this.vacations = vacations;
+	}
+
+	public Date getShiftStart() {
+		return shiftStart;
+	}
+
+	public void setShiftStart(Date shiftStart) {
+		this.shiftStart = shiftStart;
+	}
+
+	public Date getShiftEnd() {
+		return shiftEnd;
+	}
+
+	public void setShiftEnd(Date shiftEnd) {
+		this.shiftEnd = shiftEnd;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public List<Prescription> getPrescriptions() {
+		return prescriptions;
+	}
+
+	public void setPrescriptions(List<Prescription> prescriptions) {
+		this.prescriptions = prescriptions;
+	}
+
+	public Centre getCentre() {
+		return centre;
+	}
+
+	public void setCentre(Centre centre) {
+		this.centre = centre;
+	}
+
+
+	public static class Builder extends UserBuilder
+	{
+	    private Date shiftStart;
+	    private Date shiftEnd;
+	    public Centre centre;
+	    public String type;
+				
+		public Builder(String email)
+		{
+			super(email);
+		}
+		
+
+		public Builder withPassword(String password)
+		{
+			super.withPassword(password);
+			
+			return this;
+		}
+
+		public Builder withFirstname(String firstname)
+		{
+			super.withFirstname(firstname);
+			
+			return this;
+		}
+	
+		public Builder withLastname(String lastname)
+		{
+			super.withLastname(lastname);
+			
+			return this;
+		}
+		
+		public Builder withCity(String city)
+		{
+			super.withCity(city);
+			
+			return this;
+		}
+
+		
+		public Builder withState(String state)
+		{
+			super.withState(state);
+			
+			return this;
+		}
+
+		public Builder withDate_of_birth(String date_of_birth)
+		{
+			super.withDate_of_birth(date_of_birth);
+
+			return this;
+		}
+		
+		public Builder withPhone(String phone)
+		{
+			super.withPhone(phone);
+			
+			return this;
+		}
+
+		
+		public Builder withShiftStart(Date shiftStart)
+		{
+			this.shiftStart = shiftStart;
+			
+			return this;
+		}
+		
+		public Builder withShiftEnd(Date shiftEnd)
+		{
+			this.shiftEnd = shiftEnd;
+			
+			return this;
+		}
+		
+		public Builder withCentre(Centre centre)
+		{
+			this.centre = centre;
+			
+			return this;
+		}
+
+		public Builder withType(String type)
+		{
+			this.type = type;
+
+			return this;
+		}
+		
+		public Nurse build()
+		{
+			super.withRole(UserRole.Nurse);
+			User user = super.build();
+			Nurse n = new Nurse(user);
+			n.setCentre(this.centre);
+			n.setShiftStart(this.shiftStart);
+			n.setShiftEnd(this.shiftEnd);
+			n.setType(this.type);
+			return n;
+		}
+	}
+   
+}
