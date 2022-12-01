3e78a327389393248239261bc3c4898ee1f977e5
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-12-01 14:27:30 +0300

    Добавлена сущность Рецепты.

---
+++ MedicalSystemApplication/src/main/java/model/Recipe.java
@@ -0,0 +1,80 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Recipe {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+    @Column(name = "header", nullable = false)
+	private String header;
+    
+    @Column(name = "description", nullable = false)
+	private String description;
+    
+    @ManyToMany
+	private List<Drug> drugs;
+	
+    public Recipe()
+    {
+    	super();
+    }
+	
+	public Recipe(String header, String description) {
+		super();
+		this.header = header;
+		this.description = description;
+		drugs = new ArrayList<Drug>();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getHeader() {
+		return header;
+	}
+
+
+	public void setHeader(String header) {
+		this.header = header;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+
+	public List<Drug> getDrugs() {
+		return drugs;
+	}
+
+
+	public void setDrugs(List<Drug> drugs) {
+		this.drugs = drugs;
+	}
+	
+	
+	
+}
