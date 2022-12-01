b68d4643d82f0188100e6463d7ae14ae363fd127
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-12-01 14:08:52 +0300

    Добавлена сущности: CentreAdmin и CentreReview.

---
+++ MedicalSystemApplication/src/main/java/model/CentreAdmin.java
@@ -0,0 +1,144 @@
+
+package model;
+
+import javax.persistence.*;
+
+import helpers.UserBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class CentreAdmin extends User {
+
+	
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Centre centre;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<AppointmentRequest> appointmentRequests;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private  List<VacationRequest> vacationRequests;
+
+    public CentreAdmin(){
+    	setRole(UserRole.CentreAdmin);
+    	this.setIsFirstLog(true);
+    }
+
+    public CentreAdmin(String password, String email, String firstname, String lastname, String city,  String state, String date_of_birth, String phone, Centre centre) {
+        super(password, email, firstname, lastname, city, state, date_of_birth, phone, UserRole.CentreAdmin);
+        this.centre = centre;
+        this.appointmentRequests = new ArrayList<>();
+        this.vacationRequests = new ArrayList<>();
+        this.setIsFirstLog(true);
+    }
+
+    public CentreAdmin(User user)
+    {
+    	super(user);
+    	this.appointmentRequests = new ArrayList<>();
+        this.vacationRequests = new ArrayList<>();
+        this.setIsFirstLog(true);
+    }
+
+    public Centre getCentre() {
+        return centre;
+    }
+
+    public void setCentre(Centre centre) {
+        this.centre = centre;
+    }
+
+    public List<AppointmentRequest> getAppointmentRequests() {
+        return appointmentRequests;
+    }
+
+    public void setAppointmentRequests(List<AppointmentRequest> appointmentRequests) {
+        this.appointmentRequests = appointmentRequests;
+    }
+
+    public List<VacationRequest> getVacationRequests() {
+        return vacationRequests;
+    }
+
+    public void setVacationRequests(List<VacationRequest> vacationRequests) {
+        this.vacationRequests = vacationRequests;
+    }
+    
+    public static class Builder extends UserBuilder
+    {
+    	public Centre centre;
+		
+		public Builder(String email)
+		{
+			super(email);
+		}
+		
+
+		public Builder withPassword(String password)
+		{
+			super.withPassword(password);
+			
+			return this;
+		}
+
+		public Builder withFirstname(String firstname)
+		{
+			super.withFirstname(firstname);
+			
+			return this;
+		}
+	
+		public Builder withLastname(String lastname)
+		{
+			super.withLastname(lastname);
+			
+			return this;
+		}
+		
+		public Builder withCity(String city)
+		{
+			super.withCity(city);
+			
+			return this;
+		}
+
+		
+		public Builder withState(String state)
+		{
+			super.withState(state);
+			
+			return this;
+		}
+
+		public Builder withDate_of_birth(String date_of_birth) {
+			super.withDate_of_birth(date_of_birth);
+			return this;
+		}
+		
+		public Builder withPhone(String phone)
+		{
+			super.withPhone(phone);
+			
+			return this;
+		}
+		
+		public Builder withCentre(Centre centre)
+		{
+			this.centre = centre;
+			
+			return this;
+		}
+		
+		public CentreAdmin build()
+		{
+			super.withRole(UserRole.CentreAdmin);
+			User user = super.build();
+			CentreAdmin a = new CentreAdmin(user);
+			a.setCentre(this.centre);
+			return a;
+		}
+    }
+}
+
---
+++ MedicalSystemApplication/src/main/java/model/CentreReview.java
@@ -0,0 +1,69 @@
+package model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class CentreReview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+    @Column(name = "rating", nullable = false)
+    private int rating;
+
+    @Column(name = "date", nullable = false)
+    private Date date;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Patient patient;
+
+    public CentreReview(){
+
+    }
+
+        
+    public CentreReview(int rating, Date date, Patient patient) {
+		super();
+		this.rating = rating;
+		this.date = date;
+		this.patient = patient;
+	}
+
+
+
+
+	public Patient getPatient() {
+		return patient;
+	}
+
+	public void setPatient(Patient patient) {
+		this.patient = patient;
+	}
+
+	public Long getId()
+    {
+    	return id;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
