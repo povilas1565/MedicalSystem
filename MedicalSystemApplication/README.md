ae48d6b0d814910df18c89afaef7a556bfbce86d
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-12-01 13:10:48 +0300

    Начало работы над приложением Медицинской системы.

---
+++ MedicalSystemApplication/README.md
@@ -0,0 +1,27 @@
+# Medical System
+
+
+Backend application made for patients.
+
+### Technologies used:
+- **Backend:** Java + SpringBoot + PostgreSQL
+
+
+### Quick start
+#### You will need:
+
+###### For running
+- Java 8 + SpringBoot
+- Maven 3.6 or higher
+- PostgreSQL 10 or higher
+
+
+#### Creating the database
+- In order for application to run, it will need a database. Default name is set as `postgres`, running on port `5432`.
+
+### Building the project
+
+- Position to root folder
+
+ `cd MedicalSystemApplication`
+
---
+++ MedicalSystemApplication/src/main/java/com/example/MedicalSystemApplication/MedicalSystemApplication.java
@@ -0,0 +1,22 @@
+package com.example.MedicalSystemApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+
+@EntityScan("model") 
+@EnableJpaRepositories(basePackages="repository") 
+@SpringBootApplication(scanBasePackages = {"model","service","repository","controller","org.nil"})
+@EnableScheduling
+public class MedicalSystemApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(MedicalSystemApplication.class, args);		
+	}
+	
+}
