121eaca72ae49f69d02214a005c3c48c44be9e16
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-12-15 18:22:58 +0300

    Созданы api для PatientMedicalReport.

---
+++ MedicalSystemApplication/src/main/java/controller/PatientMedicalReportController.java
@@ -0,0 +1,205 @@
+package controller;
+
+import dto.PatientMedicalReportDTO;
+import dto.PrescriptionDTO;
+import model.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import service.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+    @RestController
+    @RequestMapping(value = "api/reports")
+    public class PatientMedicalReportController {
+
+        @Autowired
+        private UserService userService;
+
+        @Autowired
+        private CentreService centreService;
+
+        @Autowired
+        private DiagnosisService diagnosisService;
+
+        @Autowired
+        private PatientMedicalReportService patientMedicalReportService;
+
+        @Autowired
+        private PrescriptionService prescriptionService;
+
+        @Autowired
+        private DrugService drugService;
+
+        @Autowired
+        private MedicalRecordService medicalRecordService;
+
+
+        @GetMapping(value = "/getAllReports/{email}")
+        public ResponseEntity<List<PatientMedicalReportDTO>> getAllReports(@PathVariable("email")String email)
+        {
+            HttpHeaders header = new HttpHeaders();
+
+            Patient patient = (Patient)userService.findByEmailAndDeleted(email,false);
+            if (patient == null)
+            {
+                header.set("responseText", "patient not found: " + email);
+                return new ResponseEntity<>(header, HttpStatus.NOT_FOUND);
+            }
+
+            List<PatientMedicalReport> ret = patient.getMedicalRecord().getReports();
+
+            List<PatientMedicalReportDTO> dtos = new ArrayList<>();
+            for(PatientMedicalReport p : ret)
+            {
+                dtos.add(new PatientMedicalReportDTO(p));
+            }
+
+            System.out.println(dtos);
+
+            return new ResponseEntity<>(dtos,HttpStatus.OK);
+        }
+
+        @PutMapping(value = "/updateReport/{id}")
+        public ResponseEntity<Void> updateReport(@PathVariable("id")long id, @RequestBody PatientMedicalReportDTO dto)
+        {
+            HttpHeaders header = new HttpHeaders();
+
+            PatientMedicalReport report = patientMedicalReportService.findById(id);
+            if (report == null)
+            {
+                header.set("responseText", "report not found: " + id);
+                return new ResponseEntity<>(header, HttpStatus.NOT_FOUND);
+            }
+
+            report.getDiagnosis().clear();
+            for (String d : dto.getDiagnosis()){
+                Diagnosis diagnosis = diagnosisService.findByName(d);
+                report.getDiagnosis().add(diagnosis);
+            }
+            report.setDescription(dto.getDescription());
+
+            long idPres = report.getPrescription().getId();
+            Prescription prescription = prescriptionService.findById(idPres);
+
+            prescription.setDescription(dto.getPrescription().getDescription());
+            report.getPrescription().getDrugs().clear();
+            for (String d : dto.getPrescription().getDrugs()){
+                Drug drug = drugService.findByName(d);
+                report.getPrescription().getDrugs().add(drug);
+            }
+            prescription.setValid(false);
+            prescription.setNurse(null);
+            prescription.setValidationDate(null);
+
+            prescriptionService.save(prescription);
+
+            report.setPrescription(prescription);
+            patientMedicalReportService.save(report);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        @GetMapping(value = "/getReportPrescription/{reportId}")
+        public ResponseEntity<PrescriptionDTO> getReport(@PathVariable("reportId")long reportId)
+        {
+            HttpHeaders header = new HttpHeaders();
+
+            PatientMedicalReport report = patientMedicalReportService.findById(reportId);
+            if (report == null)
+            {
+                header.set("responseText", "report not found: " + reportId);
+                return new ResponseEntity<>(header, HttpStatus.NOT_FOUND);
+            }
+
+            Prescription ret = report.getPrescription();
+            PrescriptionDTO dto = new PrescriptionDTO(ret);
+
+            return new ResponseEntity<>(dto,HttpStatus.OK);
+        }
+
+        @PostMapping(value="/addPatientMedicalReport/{email}")
+        public ResponseEntity<Void> addPatientMedicalReport(@PathVariable("email") String email, @RequestBody PatientMedicalReportDTO dto)
+        {
+            HttpHeaders header = new HttpHeaders();
+
+            Patient patient = (Patient)userService.findByEmailAndDeleted(email,false);
+            if (patient == null)
+            {
+                header.set("responseText", "patient not found: " + email);
+                return new ResponseEntity<>(header, HttpStatus.NOT_FOUND);
+            }
+
+            Doctor doctor = (Doctor)userService.findByEmailAndDeleted(dto.getDoctorEmail(),false);
+            if (doctor == null)
+            {
+                header.set("responseText", "doctor not found: " + dto.getDoctorEmail());
+                return new ResponseEntity<>(header,HttpStatus.NOT_FOUND);
+            }
+
+            Centre centre = centreService.findByName(dto.getCentreName());
+            if (centre == null)
+            {
+                header.set("responseText", "centre not found: " + dto.getCentreName());
+                return new ResponseEntity<>(header, HttpStatus.NOT_FOUND);
+            }
+
+
+
+            Prescription pr = new Prescription();
+            pr.setDescription(dto.getPrescription().getDescription());
+            for(String name : dto.getPrescription().getDrugs())
+            {
+                Drug drug = drugService.findByName(name);
+
+                if(drug == null)
+                {
+                    header.set("responseText", "Drug not found: " + name);
+                    return new ResponseEntity<>(header,HttpStatus.NOT_FOUND);
+                }
+
+                pr.getDrugs().add(drug);
+
+            }
+            pr.setValid(false);
+            prescriptionService.save(pr);
+
+            PatientMedicalReport report = new PatientMedicalReport();
+            report.setCentre(centre);
+            report.setDescription(dto.getDescription());
+            report.setDoctor(doctor);
+            report.setPrescription(pr);
+            report.setDateAndTime(dto.getDateAndTime());
+            report.setPatient(patient);
+
+            for(String name : dto.getDiagnosis())
+            {
+                Diagnosis diagnosis = diagnosisService.findByName(name);
+
+                if(diagnosis == null)
+                {
+                    header.set("responseText", "Diagnosis not found: " + name);
+                    return new ResponseEntity<>(header,HttpStatus.NOT_FOUND);
+                }
+
+
+                report.getDiagnosis().add(diagnosis);
+            }
+
+            patientMedicalReportService.save(report);
+
+            MedicalRecord mr = medicalRecordService.findByPatient(patient);
+            mr.getReports().add(report);
+            medicalRecordService.save(mr);
+
+            userService.save(patient);
+
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+    }
+
+
