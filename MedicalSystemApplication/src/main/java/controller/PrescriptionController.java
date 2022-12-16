230d46a610c5191de641cc7d0a4a20ec0b47bcfd
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-12-16 16:08:39 +0300

    Созданы api для Назначений(Prescription).

---
+++ MedicalSystemApplication/src/main/java/controller/PrescriptionController.java
@@ -0,0 +1,73 @@
+package controller;
+
+import dto.PrescriptionDTO;
+import model.Prescription;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
+import org.springframework.web.bind.annotation.*;
+import service.PrescriptionService;
+import service.UserService;
+
+import javax.validation.ValidationException;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/prescription")
+public class PrescriptionController {
+    @Autowired
+    private PrescriptionService prescriptionService;
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(value = "/getAllPrescriptions")
+    public ResponseEntity<List<PrescriptionDTO>> getDrugs()
+    {
+        List<Prescription> prescriptions = prescriptionService.findAll();
+        List<PrescriptionDTO> prescriptionsDTO = new ArrayList<PrescriptionDTO>();
+        if(prescriptions == null)
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        for(Prescription p: prescriptions)
+        {
+            PrescriptionDTO dto = new PrescriptionDTO(p);
+            if (p.getIsValid() == false){
+                prescriptionsDTO.add(dto);
+            }
+        }
+
+        return new ResponseEntity<>(prescriptionsDTO,HttpStatus.OK);
+    }
+
+
+
+    @PutMapping(value = "/validate/{email}")
+    public ResponseEntity<Void> confirmRegister(@RequestBody PrescriptionDTO dto, @PathVariable("email") String email)
+    {
+
+        dto.setNurseEmail(email);
+
+        try
+        {
+            prescriptionService.validate(dto);
+
+        } catch (ObjectOptimisticLockingFailureException e) {
+
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+
+        } catch (ValidationException e) {
+
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+}
