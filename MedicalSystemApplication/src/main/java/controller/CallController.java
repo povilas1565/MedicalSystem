55c7fe5e7d3eb405b5fde85653d70bcd354e6284
povilas1565 <pavel.ryzhkov.99@mail.ru>
2022-12-20 12:06:56 +0300

    Созданы Api для звонков(аудио- и видео-).

---
+++ MedicalSystemApplication/src/main/java/controller/CallController.java
@@ -0,0 +1,71 @@
+package controller;
+
+import dto.CallDTO;
+import facade.CallFacade;
+import model.Call;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import response.MessageResponse;
+import service.CallService;
+import validators.ResponseErrorValidator;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value ="/api/post")
+@CrossOrigin
+public class CallController {
+
+    @Autowired
+    private CallFacade callFacade;
+
+    @Autowired
+    private CallService callService;
+
+    @Autowired
+    private ResponseErrorValidator responseErrorValidator;
+
+    @PostMapping("/create")
+    public ResponseEntity<Object> createPost(@Valid @RequestBody CallDTO callDTO, BindingResult bindingResult, Principal principal) {
+        ResponseEntity<Object> listErrors = responseErrorValidator.mappedValidatorService(bindingResult);
+        if (!ObjectUtils.isEmpty(listErrors)) return listErrors;
+
+        Call call = callService.createCall(callDTO, principal);
+        CallDTO callCreated = callFacade.callToCallDTO(call);
+
+        return new ResponseEntity<>(callCreated, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<CallDTO>> getAllCalls() {
+        List<CallDTO> callDTOList = callService.getAllCalls()
+                .stream()
+                .map(callFacade::callToCallDTO)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(callDTOList, HttpStatus.OK);
+    }
+
+    @GetMapping("/user/calls")
+    public ResponseEntity<List<CallDTO>> getAllCallsForUser(Principal principal) {
+        List<CallDTO> postDTOList = callService.getAllCallsForUser(principal)
+                .stream()
+                .map(callFacade::callToCallDTO)
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(postDTOList, HttpStatus.OK);
+    }
+
+    @PostMapping("/{callId}/delete")
+    public ResponseEntity<MessageResponse> deleteCall(@PathVariable("callId") String callId, Principal principal) {
+        callService.deleteCall(Long.parseLong(callId), principal);
+        return new ResponseEntity<>(new MessageResponse("The call" + callId + "were deleted"), HttpStatus.OK);
+    }
+}
