b8761ec8463434b15378220ef977ca61ff3f224e
povilas1565 <pavel.ryzhkov.99@mail.ru>
2023-02-21 12:31:30 +0300

    Попытка обхода внешней авторизации.

--- MedicalSystemApplication/src/main/java/com/example/MedicalSystemApplication/MedicalSystemApplication.java
+++ MedicalSystemApplication/src/main/java/com/example/MedicalSystemApplication/MedicalSystemApplication.java
@@ -2,21 +2,21 @@ package com.example.MedicalSystemApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
-@EntityScan("model") 
-@EnableJpaRepositories(basePackages="repository") 
+@EntityScan("model")
+@EnableJpaRepositories(basePackages="repository")
 @SpringBootApplication(scanBasePackages = {"model","service","repository","controller","org.nil"})
 @EnableScheduling
 public class MedicalSystemApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(MedicalSystemApplication.class, args);		
+		ConfigurableApplicationContext ctx = SpringApplication.run(MedicalSystemApplication.class, args);
 	}
-	
+
 }
