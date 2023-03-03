84466c8855c7130540afb8cde945416dc7e38dca
MetDeYan <104005991+MetDeYan@users.noreply.github.com>
2023-03-03 16:12:39 +0300

    Add logs
    Add logs to file

--- MedicalSystemApplication/src/main/java/controller/UserController.java
+++ MedicalSystemApplication/src/main/java/controller/UserController.java
@@ -4,6 +4,7 @@ import dto.*;
 import helpers.SecurePasswordHasher;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import model.*;
 import model.User.UserRole;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +21,7 @@ import java.util.List;
 @RequestMapping(value = "api/users")
 @CrossOrigin
 @Api
+@Slf4j
 public class UserController {
 
     @Autowired
@@ -46,6 +48,7 @@ public class UserController {
     @PutMapping(value = "/update/{email}")
     @ApiOperation("Обновление(добавление, изменение) данных пользователя")
     public ResponseEntity<Void> updateUser(@RequestBody UserDTO dto, @PathVariable("email") String email) {
+        log.info("Changing user data with email '{}'.", email);
         User user = userService.findByEmailAndDeleted(email, false);
 
         if (user != null) {
@@ -66,6 +69,7 @@ public class UserController {
     @PutMapping(value = "/update/password/{email}", consumes = "application/json")
     @ApiOperation("Обновление(изменение) пароля")
     public ResponseEntity<Void> updatePassword(@PathVariable("email") String email, @RequestBody PasswordDTO dto) {
+        log.info("Changing the password of a user with email '{}'.", email);
         System.out.println(email);
         User user = userService.findByEmailAndDeleted(email, false);
 
@@ -93,6 +97,7 @@ public class UserController {
     @PutMapping(value = "/update/firstPassword/{email}", consumes = "application/json")
     @ApiOperation("Обновление(изменение) пароля")
     public ResponseEntity<Void> updateFirstPassword(@PathVariable("email") String email, @RequestBody PasswordDTO dto) {
+        log.info("Adding user password with email '{}'.", email);
         User user = userService.findByEmailAndDeleted(email,false);
         if (user != null) {
             String newPassword = dto.getNewPassword();
@@ -113,6 +118,7 @@ public class UserController {
     @GetMapping(value = "/getPatient/{email}")
     @ApiOperation("Получение данных пациентов")
     public ResponseEntity<UserDTO> getPatient(@PathVariable("email") String email) {
+        log.info("Getting data of patients by email '{}'.", email);
         Patient ret = (Patient) userService.findByEmailAndDeleted(email, false);
 
         if (ret == null || ret.getDeleted()) {
@@ -125,6 +131,7 @@ public class UserController {
     @GetMapping(value = "/getDoctor/{email}")
     @ApiOperation("Получение данных докторов")
     public ResponseEntity<DoctorDTO> getDoctor(@PathVariable("email") String email)  {
+        log.info("Getting data of doctors by email '{}'.", email);
         Doctor ret = (Doctor) userService.findByEmailAndDeleted(email, false);
 
         if (ret == null || ret.getDeleted()) {
@@ -137,6 +144,7 @@ public class UserController {
     @GetMapping(value = "/getNurse/{email}")
     @ApiOperation("Получение данных медработников")
     public ResponseEntity<NurseDTO> getNurse(@PathVariable("email") String email) {
+        log.info("Getting data of nurses by email '{}'.", email);
         Nurse ret = (Nurse) userService.findByEmailAndDeleted(email, false);
 
         if (ret == null || ret.getDeleted()) {
@@ -147,8 +155,9 @@ public class UserController {
     }
 
     @GetMapping(value="/getCentreAdmin/{email}")
-    @ApiOperation("Получение данных администраторв центров")
+    @ApiOperation("Получение данных администраторов центров")
     public ResponseEntity<UserDTO> getCentreAdmin(@PathVariable("email") String email) {
+        log.info("Getting data of Center Administrator by email '{}'.", email);
         User ret = (User) userService.findByEmailAndDeleted(email, false);
 
         if (ret == null || ret.getDeleted()) {
@@ -173,6 +182,7 @@ public class UserController {
     @GetMapping(value = "/{userId}")
     @ApiOperation("Получение профилей пользователей по Id")
     public ResponseEntity<UserDTO> getUserProfile(@PathVariable("userId") String userId) {
+        log.info("Getting a user with id '{}'.", userId);
         User ret = userService.findUserById(Long.parseLong(userId));
 
         if (ret == null || ret.getDeleted()) {
@@ -186,7 +196,7 @@ public class UserController {
     @GetMapping(value = "/getAll/{role}")
     @ApiOperation("Получение всех пользователей по конкретным role")
     public ResponseEntity<List<UserDTO>> geAllUserByRole(@PathVariable("role") UserRole role) {
-
+        log.info("Getting all users by role '{}'.", role);
         List<User> ret = userService.getAll(role);
         List<UserDTO> dtos = new ArrayList<UserDTO>();
         for (User u : ret) {
@@ -205,6 +215,7 @@ public class UserController {
     @GetMapping(value = "/getAll")
     @ApiOperation("Получение всех пользователей")
     public ResponseEntity<List<UserDTO>> getAllUsers() {
+        log.info("Getting all users.");
         List<User> ret = userService.getAll();
         List<UserDTO> dtos = new ArrayList<UserDTO>();
         for (User u : ret) {
@@ -223,6 +234,7 @@ public class UserController {
     @GetMapping(value = "/patient/getMedicalRecord/{email}")
     @ApiOperation("Получение медкарт пациентов")
     public ResponseEntity<MedicalRecordDTO> getMedicalRecord(@PathVariable("email") String email) {
+        log.info("Getting patient medical records by email '{}'.", email);
         Patient patient = (Patient) userService.findByEmailAndDeleted(email, false);
 
         if (patient == null) {
@@ -238,6 +250,7 @@ public class UserController {
     @PutMapping(value = "/patient/updateMedicalRecord/{email}")
     @ApiOperation("Обновление(изменение) данных медкарт пациентов")
     ResponseEntity<Void> updateMedicalRecord(@PathVariable("email") String email, @RequestBody MedicalRecordDTO dto) {
+        log.info("Changing the data of the patient's medical record with the email '{}'.", email);
         Patient patient = (Patient) userService.findByEmailAndDeleted(email, false);
         MedicalRecord record = medicalRecordService.findByPatient(patient);
 
