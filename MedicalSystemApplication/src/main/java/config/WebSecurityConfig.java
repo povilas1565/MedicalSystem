e5adf9d392bf4d54053483a9293b652c5fb1e2a1
povilas1565 <pavel.ryzhkov.99@mail.ru>
2023-03-06 22:37:07 +0300

    Добавлено отключения корса и csrf, а также добавлена sessionCreationPolicy.

--- MedicalSystemApplication/src/main/java/config/WebSecurityConfig.java
+++ MedicalSystemApplication/src/main/java/config/WebSecurityConfig.java
@@ -4,13 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.cors().and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests()
                 .antMatchers("/**").permitAll();
     }
