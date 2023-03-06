aa066afee4eb1697b4170384de076b522e06c275
povilas1565 <pavel.ryzhkov.99@mail.ru>
2023-03-06 23:21:29 +0300

    Отключены cors и csrf.

--- MedicalSystemApplication/src/main/java/config/WebSecurityConfig.java
+++ MedicalSystemApplication/src/main/java/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
@@ -13,9 +14,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll();
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated();
+
     }
 }
