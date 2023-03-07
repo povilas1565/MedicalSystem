a92365a547c71dd543664f114d578fbdb3315847
povilas1565 <pavel.ryzhkov.99@mail.ru>
2023-03-07 12:02:10 +0300

    Изменение версии spring boot 2.5.6 на 2.2.1.RELEASE

--- MedicalSystemApplication/src/main/java/config/WebSecurityConfig.java
+++ MedicalSystemApplication/src/main/java/config/WebSecurityConfig.java
@@ -15,8 +15,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
-                .anyRequest().authenticated();
+                .antMatchers("/**").permitAll();
 
     }
 }
