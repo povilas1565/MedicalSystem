5b42e029376579ebdf9e09e1b9ec211408dd259e
povilas1565 <pavel.ryzhkov.99@mail.ru>
2023-03-08 19:07:31 +0300

    Commit Changes

--- MedicalSystemApplication/src/main/java/config/WebSecurityConfig.java
+++ MedicalSystemApplication/src/main/java/config/WebSecurityConfig.java
@@ -10,8 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-            http.
-                    csrf().disable()
+            http
+                    .csrf().disable()
                     .authorizeRequests()
                     .antMatchers("/**").permitAll();
     }
