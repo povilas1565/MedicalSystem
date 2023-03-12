21c22a74a588c39e53f80d62183b9c810a5fa282
povilas1565 <pavel.ryzhkov.99@mail.ru>
2023-03-12 19:00:12 +0300

    Выключен cors.

--- MedicalSystemApplication/src/main/java/config/WebSecurityConfig.java
+++ MedicalSystemApplication/src/main/java/config/WebSecurityConfig.java
@@ -11,7 +11,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
             http
-                    .csrf().and().cors().disable()
+                    .csrf().disable()
                     .authorizeRequests()
                     .antMatchers("/**").permitAll();
     }
