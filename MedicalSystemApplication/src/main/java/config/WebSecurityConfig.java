16f3d4489d9bb0a6a00c68d0296428a500728e2d
povilas1565 <pavel.ryzhkov.99@mail.ru>
2023-03-12 22:51:03 +0300

    Commit Changes.

--- MedicalSystemApplication/src/main/java/config/WebSecurityConfig.java
+++ MedicalSystemApplication/src/main/java/config/WebSecurityConfig.java
@@ -1,6 +1,9 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,4 +18,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .authorizeRequests()
                     .antMatchers("/**").permitAll();
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authManager) throws Exception {
+        // This is the code you usually have to configure your authentication manager.
+        // This configuration will be used by authenticationManagerBean() below.
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        // ALTHOUGH THIS SEEMS LIKE USELESS CODE,
+        // IT'S REQUIRED TO PREVENT SPRING BOOT AUTO-CONFIGURATION
+        return super.authenticationManagerBean();
+    }
+
 }
+
