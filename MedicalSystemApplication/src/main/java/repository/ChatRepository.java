b89303a8b2cf789a5a54073bdb5a0a1dbaa9a0e1
povilas1565 <pavel.ryzhkov.99@mail.ru>
2023-03-18 23:08:06 +0300

    Commit Changes.

--- MedicalSystemApplication/src/main/java/repository/ChatRepository.java
+++ MedicalSystemApplication/src/main/java/repository/ChatRepository.java
@@ -6,12 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
-    Chat findByDoctor(Doctor doctor);
-
-    Chat findByPatient(Patient patient);
-
-    Chat findByNurse(Nurse nurse);
-
     Chat findByDoctorAndPatient(Doctor doctor, Patient patient);
 
     Chat findByNurseAndPatient(Nurse nurse, Patient patient);
