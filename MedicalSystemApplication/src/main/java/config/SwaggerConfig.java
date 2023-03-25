319d43897dad68df6ccc9286248cec1e868ed367
povilas1565 <pavel.ryzhkov.99@mail.ru>
2023-03-25 17:54:43 +0300

    Попытка предоставить полную обзорную документацию в Swagger.

--- MedicalSystemApplication/src/main/java/config/SwaggerConfig.java
+++ MedicalSystemApplication/src/main/java/config/SwaggerConfig.java
@@ -6,17 +6,13 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-
-
-
-
 @Configuration
 public class SwaggerConfig {
         @Bean
         public Docket api() {
                 return new Docket(DocumentationType.SWAGGER_2)
                         .select()
-                        .apis(RequestHandlerSelectors.basePackage("controller"))
+                        .apis(RequestHandlerSelectors.any())
                         .paths(PathSelectors.any())
                         .build();
         }
--- MedicalSystemApplication/src/main/java/config/WebSecurityConfig.java
+++ MedicalSystemApplication/src/main/java/config/WebSecurityConfig.java
@@ -3,8 +3,10 @@ package config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
