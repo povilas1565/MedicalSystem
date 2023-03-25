5dab95335f848ab4d07abea018bde7150bcd30d4
povilas1565 <pavel.ryzhkov.99@mail.ru>
2023-03-25 18:58:34 +0300

    Пофикшены баги при работе и загрузкой фоток профилей пользователей.

--- MedicalSystemApplication/src/main/java/controller/ImageController.java
+++ MedicalSystemApplication/src/main/java/controller/ImageController.java
@@ -3,17 +3,12 @@ package controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import model.Image;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import response.MessageResponse;
 import service.ImageService;
-
 import java.io.IOException;
-import java.security.Principal;
 
 @Slf4j
 @RestController
@@ -27,18 +22,17 @@ public class ImageController {
 
     @PostMapping("/upload")
     @ApiOperation("Загрузка фоток для профилей пользователей")
-    public ResponseEntity<MessageResponse> uploadImageToProfile(@RequestParam("file") MultipartFile file,
-                                                                Principal principal) throws IOException {
-        log.info("Uploading a photo for the user profile '{}'.", principal.getName());
-        imageService.uploadImageToProfile(file, principal);
-        return ResponseEntity.ok(new MessageResponse("Image upload successfully"));
+    public ResponseEntity<String> uploadImageToProfile(@RequestParam("id") Long id,
+                                                       @RequestParam("file") MultipartFile file) throws IOException {
+        log.info("Upload a photo for the user profile with id '{}'.", id);
+        imageService.uploadImageToProfile(id, file);
+        return ResponseEntity.ok("Image upload successfully");
     }
 
     @GetMapping("/profileImage")
     @ApiOperation("Получение фоток профилей пользователей")
-    public ResponseEntity<Image> getUserProfileImage(Principal principal) {
-        log.info("Get photo for user profile '{}'.", principal.getName());
-        Image profileImage = imageService.getUserProfileImage(principal);
-        return new ResponseEntity<>(profileImage, HttpStatus.OK);
+    public ResponseEntity<byte[]> getUserProfileImage(@RequestParam("id") Long id) {
+        log.info("Get photo for user profile with id '{}'.", id);
+        return imageService.getUserProfileImage(id);
     }
 }
--- MedicalSystemApplication/src/main/java/service/ImageService.java
+++ MedicalSystemApplication/src/main/java/service/ImageService.java
@@ -1,20 +1,17 @@
 package service;
 
 import model.Image;
-import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
 import org.springframework.web.multipart.MultipartFile;
 import repository.ImageRepository;
-import repository.UserRepository;
-
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.security.Principal;
+import java.util.Optional;
 import java.util.zip.DataFormatException;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
@@ -25,41 +22,30 @@ public class ImageService {
     public static final Logger LOG = LoggerFactory.getLogger(ImageService.class);
 
     private final ImageRepository imageRepository;
-    private final UserRepository userRepository;
 
     @Autowired
-    public ImageService(ImageRepository imageRepository, UserRepository userRepository) {
+    public ImageService(ImageRepository imageRepository) {
         this.imageRepository = imageRepository;
-        this.userRepository = userRepository;
     }
 
-    public void uploadImageToProfile(MultipartFile file, Principal principal) throws IOException {
-        User user = getUserByPrincipal(principal);
-        Image userProfileImage = imageRepository.findById(user.getId()).orElse(null);
-
-        // проверяем - есть ли уже фотография пользователя в базе.
-        // если есть - тогда удаляем и загружаем новую
-        if (!ObjectUtils.isEmpty(userProfileImage)) {
-            imageRepository.delete(userProfileImage);
-        }
-
+    public void uploadImageToProfile(long id, MultipartFile file) throws IOException {
         Image image = new Image();
-        image.setId(user.getId());
+        image.setId(id);
+        image.setName(file.getOriginalFilename());
         image.setImageBytes(compressImage(file.getBytes()));
-        image.setName(file.getName());
-        LOG.info("Create image to user {}", user.getId());
-
+        LOG.info("Create image to user {}", id);
         imageRepository.save(image);
     }
 
-    public Image getUserProfileImage(Principal principal) {
-        User user = getUserByPrincipal(principal);
+    public ResponseEntity<byte[]> getUserProfileImage(Long id) {
 
-        Image userProfileImage = imageRepository.findById(user.getId()).orElse(null);
-        if (!ObjectUtils.isEmpty(userProfileImage)) {
-            userProfileImage.setImageBytes(decompressImage(userProfileImage.getImageBytes()));
+        Optional<Image> userProfileImage = imageRepository.findById(id);
+        byte[] imageBytes = null;
+        if (userProfileImage.isPresent()) {
+            imageBytes = decompressImage(userProfileImage.get().getImageBytes());
         }
-        return userProfileImage;
+
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(imageBytes);
     }
 
     public static byte[] compressImage(byte[] data) {
@@ -99,12 +85,6 @@ public class ImageService {
             LOG.error(("cannot decompress image"));
         }
         return byteArrayOutputStream.toByteArray();
-
     }
 
-    private User getUserByPrincipal(Principal principal) {
-        String username = principal.getName();
-        return userRepository.findUserByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found with username " + username));
-    }
 }
